1ad0858278a9c75ce30620208d5a191fc4593934
MaidSafe-QA <qa@maidsafe.net>
2016-04-23 10:54:10 +0000

    Updated documentation.

--- master/implementors/core/str/pattern/trait.Searcher.js
+++ master/implementors/core/str/pattern/trait.Searcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['regex_syntax'] = [];implementors['regex'] = [];
+implementors['libc'] = [];implementors['regex_syntax'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
