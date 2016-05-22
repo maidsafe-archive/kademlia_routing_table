6a04139a0e5b0c1b260d4c1b55286823b14e2148
MaidSafe-QA <qa@maidsafe.net>
2016-05-22 19:28:44 +0000

    Updated documentation.

---
+++ master/implementors/kademlia_routing_table/trait.ContactInfo.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['kademlia_routing_table'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
